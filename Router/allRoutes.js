369b70c1e1ff353ac5f41b8f63a6ec75bbd246dd
juan-gil-05 <gilj06@hotmail.com>
2024-12-16 22:38:21 +0100

    Création des fichiers html et scss de la page; et création de la route ver la page des covoiturages

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/page-accueil.html"), // Page d'accueil
-    new Route("/index.html", "Accueil", "/pages/page-accueil.html") // Page d'accueil
+    new Route("/index.html", "Accueil", "/pages/page-accueil.html"), // Page d'accueil
+    new Route("/covoiturages", "Covoiturages", "/pages/page-vue-covoiturages.html") // Page de la vue des covoiturages
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
