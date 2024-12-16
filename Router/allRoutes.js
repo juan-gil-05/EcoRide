e6915504a0919c15a85fc8cf7317f9445ef0b971
juan-gil-05 <gilj06@hotmail.com>
2024-12-16 22:16:57 +0100

    Ajout de la route vers la page daccueil depuis index.html

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/page-accueil.html"),];
+    new Route("/", "Accueil", "/pages/page-accueil.html"), // Page d'accueil
+    new Route("/index.html", "Accueil", "/pages/page-accueil.html") // Page d'accueil
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "EcoRide";
