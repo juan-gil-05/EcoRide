963263d722de98178436cdefbb0dd02f2a8f46a0
juan-gil-05 <gilj06@hotmail.com>
2024-12-24 15:00:21 +0100

    Création de la page .html, du fichier .scss, et ajout de la route vers la page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/accueil", "Accueil", "/pages/page-accueil.html"), // Page d'accueil
     new Route("/index.html", "Accueil", "/pages/page-accueil.html"), // Page d'accueil
-    new Route("/covoiturages", "Covoiturages", "/pages/page-vue-covoiturages.html", "/Scripts/covoiturages-filter.js") // Page de la vue des covoiturages
+    new Route("/covoiturages", "Covoiturages", "/pages/page-vue-covoiturages.html", "/Scripts/covoiturages-filter.js"), // Page de la vue des covoiturages
+    new Route("/detaille-covoiturage", "Détaillés du covoiturage", "/pages/page-covoiturage-detaille.html") // Page de la vue détaillée des covoiturages
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
