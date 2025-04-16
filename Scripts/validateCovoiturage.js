cd6b6173a86db237acce8a00428076543402e1a8
juan-gil-05 <gilj06@hotmail.com>
2025-04-16 15:18:00 +0200

    Refactor dans l'affichage de la vue quand le passager indique que le covoiturage s'est mal passé

--- Scripts/validateCovoiturage.js
+++ Scripts/validateCovoiturage.js
@@ -3,14 +3,18 @@
 const yesCheckedRadio = document.getElementById("yesCheckedRadio");
 const noCheckedRadio = document.getElementById("noCheckedRadio");
 const commentAboutTravel = document.getElementById("commentAboutTravel");
+const driverNote = document.getElementById("driverNote");
+const driverComment = document.getElementById("driverComment");
 
 yesCheckedRadio.addEventListener("click", function () {
     commentAboutTravel.classList.remove("show");
-    driverNote.classList.add("show");
+    driverNote.classList.remove("hidden");
+    driverComment.classList.remove("hidden")
 })
 
 noCheckedRadio.addEventListener("click", function () {
     commentAboutTravel.classList.add("show");
-    driverNote.classList.remove("show");
+    driverNote.classList.add("hidden");
+    driverComment.classList.add("hidden")
 })
 
