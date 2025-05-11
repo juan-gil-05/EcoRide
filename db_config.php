b19bed3d516db0aac1888ac2ea730e8ca79e3dca
juan-gil-05 <gilj06@hotmail.com>
2025-05-11 22:39:58 +0200

    Suppresion du db_config pour la sécurité

--- db_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Paramètres de la Base de données
-    return [
-        'db_name' => 'ecorideDB',
-        'db_user' => 'juandb',
-        'db_password' => 'bFAFOdhT9zcPuPz',
-        'db_port' => '3306',
-        'db_host' => 'localhost',  
-    ];
