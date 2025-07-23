4d66c5ec930f29fd91e97da3422d2d4e44a75c59
juan-gil-05 <gilj06@hotmail.com>
2025-07-23 10:06:42 +0200

    refactor style page du contact

--- Templates/Page/contact.php
+++ Templates/Page/contact.php
@@ -4,36 +4,39 @@ require_once  BASE_PATH . '/Templates/header.php';
 ?>
 
 <!-- main -->
-<section class="container mt-5 mb-5">
-    <div class="contact-div">
-        <!-- Formulaire pour envoyer le message -->
-        <form method="post" class="contact-form">
-            <!-- Titre -->
-            <h2 class="subtitle-text fs-4 text-center">Contactez nous</h2>
-            <!-- Pseudo et Email-->
-            <div class="d-flex user-info">
-                <!-- Pseudo -->
-                <div class="content-text">
-                    <label for="Pseudo" class="form-labe">Pseudo</label>
-                    <input type="text" name="Pseudo" id="Pseudo" class="form-control" placeholder="Juanito" required>
-                </div>
-                <!-- Email -->
-                <div class="content-text">
-                    <label for="email" class="form-labe">Email</label>
-                    <input type="email" name="email" id="email" class="form-control"
-                        placeholder="juanito@gmail.com" required>
-                </div>
+<section class="container my-5 bg-light shadow-lg rounded-4 p-4 p-md-5">
+    <form method="post" class="contact-form">
+        <!-- Titre -->
+        <h2 class="text-primary headline-text fw-bold mb-4 text-center">Contactez nous</h2>
+        <!-- Pseudo et Email-->
+        <div class="row g-4">
+            <!-- Pseudo -->
+            <div class="col-md-6">
+                <label for="Pseudo" class="form-label text-dark content-text fw-semibold">Pseudo</label>
+                <input type="text" name="Pseudo" id="Pseudo"
+                    class="form-control bg-light small-text form-control-lg shadow-sm"
+                    placeholder="Juanito" required>
             </div>
-            <!-- Message -->
-            <div class="content-text">
-                <label for="message" class="form-labe">Message</label>
-                <textarea name="message" id="message" class="form-control"
-                    placeholder="Votre message" required></textarea>
+            <!-- Email -->
+            <div class="col-md-6">
+                <label for="email" class="form-label text-dark content-text fw-semibold">Email</label>
+                <input type="email" name="email" id="email"
+                    class="form-control bg-light small-text form-control-lg shadow-sm"
+                    placeholder="juanito@gmail.com" required>
             </div>
-            <!-- Bouton pour envoyer le formulaire -->
-            <button type="submit" class="btn btn-warning secondary-btn">Envoyer</button>
-        </form>
-    </div>
+        </div>
+        <!-- Message -->
+        <div class="content-text mt-4">
+            <label for="message" class="form-label text-dark content-text fw-semibold">Message</label>
+            <textarea name="message" id="message" class="form-control bg-light small-text form-control-lg shadow-sm"
+                placeholder="Votre message" required></textarea>
+        </div>
+        <!-- Bouton pour envoyer le formulaire -->
+        <button type="submit" class="btn btn-warning btn-lg text-dark content-text fw-semibold 
+        w-50 mx-auto d-block mt-4">
+            Envoyer
+        </button>
+    </form>
 </section>
 
 
